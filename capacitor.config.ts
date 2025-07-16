92953521b3237022b4043227ffc54b5fa700995d
SonJefer <jeferson.ospinoo@pca.edu.co>
2025-07-16 16:42:45 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Singer_PCA',
+  webDir: 'www'
+};
+
+export default config;
