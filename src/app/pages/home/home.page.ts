4492d29314e901da54cece21f67fc4cf431b7bbd
SonJefer <jeferson.ospinoo@pca.edu.co>
2025-07-27 18:48:46 -0500

    Cambio Registro y Mostrar Artistas

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton, IonIcon} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { contrastOutline, logoIonic } from 'ionicons/icons';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonButton, IonIcon  ],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonButton, IonIcon, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePage {
+  slider_artista_content = [
+    
+    {
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/BillieEilishO2160622_%2819_of_45%29_%2852153214339%29_%28cropped_3%29.jpg/960px-BillieEilishO2160622_%2819_of_45%29_%2852153214339%29_%28cropped_3%29.jpg",
+    title: "Prueba 1",
+    subtitle: ""
+
+    }
+  ]
   constructor() {
     addIcons({ logoIonic, contrastOutline });
   }
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,18 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent,  IonButton,IonInput,IonItem,  IonLabel, IonList} from '@ionic/angular/standalone';
+import { FormsModule,  FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { eye, lockClosed, logoGoogle, logoFacebook } from 'ionicons/icons';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonContent,  CommonModule, FormsModule, IonButton,IonInput, IonItem,  IonLabel, IonList ]
+  imports: [ CommonModule, FormsModule, IonicModule, ReactiveFormsModule ]
 })
 export class RegisterPage implements OnInit {
+  miFormulario: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.miFormulario = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+    addIcons({ eye, lockClosed, logoGoogle, logoFacebook });
+  }
+
+   submit() {
+    if (this.miFormulario.valid) {
+      console.log('✅ Formulario válido:', this.miFormulario.value);
+    } else {
+      console.log('❌ Formulario inválido');
+      this.miFormulario.markAllAsTouched(); // Fuerza la visualización de errores
+    }
+  }
 
   ngOnInit() {
   }
